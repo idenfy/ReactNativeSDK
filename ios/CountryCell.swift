6af1217047f5fe3e522282cf76300b837890b9ec
Viktor Vostrikov <69972319+viktor-idenfy@users.noreply.github.com>
2022-08-30 16:57:57 +0300

    customization-finalization

---
+++ ios/CountryCell.swift
@@ -0,0 +1,92 @@
+//
+//  CountryCell.swift
+//  testing
+//
+//  Created by Viktas Juškys on 2022-08-25.
+//  Copyright © 2022 iDenfy. All rights reserved.
+//
+
+import Foundation
+import idenfycore
+import iDenfySDK
+import idenfyviews
+import Lottie
+
+@objc open class CountryCell: UITableViewCell, CountryCellViewable {
+    public var hasBorder = false
+
+    public let cellView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    public var countryLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = IdenfyCountrySelectionViewUISettingsV2.idenfyCountrySelectionViewCountryTableViewCellFont
+        label.textAlignment = .left
+        label.textColor = IdenfyCountrySelectionViewUISettingsV2.idenfyCountrySelectionViewCountryTableViewCellTextColor
+        return label
+    }()
+
+    public var loadingSpinner: AnimationView = {
+        let lottieView = AnimationView(frame: .zero)
+        lottieView.translatesAutoresizingMaskIntoConstraints = false
+        if let anim = Bundle(identifier: "com.idenfy.idenfyviews")!.path(forResource: "idenfy_custom_country_loader", ofType: "json") {
+            lottieView.animation = Animation.filepath(anim)
+        }
+        lottieView.contentMode = .scaleAspectFit
+        lottieView.play()
+        lottieView.loopMode = .loop
+        lottieView.isHidden = true
+        return lottieView
+    }()
+
+    public var countryImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.isOpaque = true
+        imageView.layer.cornerRadius = 18
+        imageView.layer.masksToBounds = true
+        return imageView
+    }()
+
+    override public init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupView()
+    }
+
+    public required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupView() {
+        addSubview(cellView)
+        cellView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        cellView.rightAnchor.constraint(equalTo: safeRightAnchor).isActive = true
+        cellView.leftAnchor.constraint(equalTo: safeLeftAnchor).isActive = true
+        
+        
+        cellView.addSubview(countryImageView)
+        countryImageView.rightAnchor.constraint(equalTo: cellView.safeRightAnchor, constant: -16).isActive = true
+        countryImageView.centerYAnchor.constraint(equalTo: cellView.centerYAnchor).isActive = true
+        countryImageView.widthAnchor.constraint(equalToConstant: 36).isActive = true
+        countryImageView.heightAnchor.constraint(equalToConstant: 36).isActive = true
+
+        cellView.addSubview(loadingSpinner)
+        loadingSpinner.topAnchor.constraint(equalTo: cellView.safeTopAnchor).isActive = true
+        loadingSpinner.bottomAnchor.constraint(equalTo: cellView.safeBottomAnchor).isActive = true
+        loadingSpinner.rightAnchor.constraint(equalTo: countryImageView.leftAnchor, constant: -8).isActive = true
+        loadingSpinner.widthAnchor.constraint(equalToConstant: 25).isActive = true
+        loadingSpinner.heightAnchor.constraint(equalToConstant: 25).isActive = true
+
+        cellView.addSubview(countryLabel)
+        countryLabel.leftAnchor.constraint(equalTo: cellView.safeLeftAnchor, constant: 16).isActive = true
+        countryLabel.rightAnchor.constraint(equalTo: cellView.safeRightAnchor, constant: -(frame.width * 0.3)).isActive = true
+        countryLabel.topAnchor.constraint(equalTo: cellView.safeTopAnchor).isActive = true
+        countryLabel.bottomAnchor.constraint(equalTo: cellView.safeBottomAnchor).isActive = true
+        countryLabel.centerYAnchor.constraint(equalTo: cellView.centerYAnchor).isActive = true
+    }
+}
--- ios/IdenfyReactNative.swift
+++ ios/IdenfyReactNative.swift
@@ -3,7 +3,7 @@ import iDenfySDK
 
 @objc(IdenfyReactNative)
 class IdenfyReactNative: NSObject {
-
+    
     @objc func start(_ config: NSDictionary,
                      resolver resolve: @escaping RCTPromiseResolveBlock,
                      rejecter reject: @escaping RCTPromiseRejectBlock) {
@@ -11,15 +11,15 @@ class IdenfyReactNative: NSObject {
             self.run(withConfig: config, resolver: resolve, rejecter: reject)
         }
     }
-
+    
     @objc func startFaceReAuth(_ config: NSDictionary,
-                         resolver resolve: @escaping RCTPromiseResolveBlock,
-                         rejecter reject: @escaping RCTPromiseRejectBlock) {
-            DispatchQueue.main.async {
-                self.runFaceReauth(withConfig: config, resolver: resolve, rejecter: reject)
-            }
+                               resolver resolve: @escaping RCTPromiseResolveBlock,
+                               rejecter reject: @escaping RCTPromiseRejectBlock) {
+        DispatchQueue.main.async {
+            self.runFaceReauth(withConfig: config, resolver: resolve, rejecter: reject)
         }
-
+    }
+    
     private func run(withConfig config: NSDictionary,
                      resolver resolve: @escaping RCTPromiseResolveBlock,
                      rejecter reject: @escaping RCTPromiseRejectBlock) {
@@ -28,18 +28,44 @@ class IdenfyReactNative: NSObject {
             let idenfySettingsV2 = IdenfyBuilderV2()
                 .withAuthToken(authToken)
                 .build()
-
+            
+            
+            let idenfyColorMain = "#EA9619"
+            let idenfyColorButton = "#6539AC"
+            IdenfyCommonColors.idenfyMainColorV2 = UIColor(hexString: idenfyColorMain)
+            IdenfyCommonColors.idenfyMainDarkerColorV2 = UIColor(hexString: idenfyColorMain)
+            IdenfyCommonColors.idenfyGradientColor1V2 = UIColor(hexString: idenfyColorButton)
+            IdenfyCommonColors.idenfyGradientColor2V2 = UIColor(hexString: idenfyColorButton)
+            
+            IdenfyToolbarUISettingsV2.idenfyDefaultToolbarBackgroundColor = UIColor(hexString: idenfyColorMain)
+            
+            IdenfyToolbarUISettingsV2.idenfyDefaultToolbarBackIconTintColor = IdenfyCommonColors.idenfyBlack
+            IdenfyToolbarUISettingsV2.idenfyDefaultToolbarLogoIconTintColor = IdenfyCommonColors.idenfyBlack
+            
+            IdenfyToolbarUISettingsV2.idenfyLanguageSelectionToolbarLanguageSelectionIconTintColor  = IdenfyCommonColors.idenfyBlack
+            IdenfyToolbarUISettingsV2.idenfyLanguageSelectionToolbarCloseIconTintColor = IdenfyCommonColors.idenfyBlack
+            
+            IdenfyCommonColors.idenfyPhotoResultDetailsCardBackgroundColorV2 = UIColor(hexString: "#FFE5BD")
+            
+            IdenfyPhotoResultViewUISettingsV2.idenfyPhotoResultViewDetailsCardTitleColor = UIColor(hexString: idenfyColorButton)
+            
+            let idenfyViewsV2:IdenfyViewsV2 = IdenfyViewsBuilderV2()
+                .withCountryCellView(CountryCell.self)
+                .build()
+            
+            
+            
             let idenfyController = IdenfyController.shared
-            idenfyController.initializeIdenfySDKV2WithManual(idenfySettingsV2: idenfySettingsV2)
-
+            idenfyController.initializeIdenfySDKV2WithManual(idenfySettingsV2: idenfySettingsV2, idenfyViewsV2: idenfyViewsV2)
+            
             let idenfyVC = idenfyController.instantiateNavigationController()
-
+            
             idenfyVC.modalPresentationStyle = .fullScreen
-
+            
             UIApplication.shared.windows.first?.rootViewController?.present(idenfyVC, animated: true)
-
+            
             handleSdkCallbacks(idenfyController: idenfyController, resolver: resolve)
-
+            
         } catch let error as NSError {
             reject("error", error.domain, error)
             return
@@ -48,49 +74,49 @@ class IdenfyReactNative: NSObject {
             return
         }
     }
-
+    
     private func handleSdkCallbacks(idenfyController: IdenfyController, resolver resolve: @escaping RCTPromiseResolveBlock) {
         idenfyController.handleIdenfyCallbacksWithManualResults(idenfyIdentificationResult: {
             idenfyIdentificationResult
-                in
+            in
             let response = NativeResponseToReactNativeResponseMapper.map(o: idenfyIdentificationResult)
             resolve(response)
         })
     }
-
+    
     private func runFaceReauth(withConfig config: NSDictionary,
-                         resolver resolve: @escaping RCTPromiseResolveBlock,
-                         rejecter reject: @escaping RCTPromiseRejectBlock) {
-            do {
-                let authToken = GetSdkConfig.getAuthToken(config: config)
-
-                let idenfyController = IdenfyController.shared
-                let faceReauthenticationInitialization = FaceAuthenticationInitialization(authenticationToken: authToken, withImmediateRedirect: false)
-                idenfyController.initializeFaceAuthentication(faceAuthenticationInitialization: faceReauthenticationInitialization)
-
-                let idenfyVC = idenfyController.instantiateNavigationController()
-
-                idenfyVC.modalPresentationStyle = .fullScreen
-
-                UIApplication.shared.windows.first?.rootViewController?.present(idenfyVC, animated: true)
-
-                handleFaceReauthSdkCallbacks(idenfyController: idenfyController, resolver: resolve)
-
-            } catch let error as NSError {
-                reject("error", error.domain, error)
-                return
-            } catch {
-                reject("error", "Unexpected error. Verify that config is structured correctly.", error)
-                return
-            }
+                               resolver resolve: @escaping RCTPromiseResolveBlock,
+                               rejecter reject: @escaping RCTPromiseRejectBlock) {
+        do {
+            let authToken = GetSdkConfig.getAuthToken(config: config)
+            
+            let idenfyController = IdenfyController.shared
+            let faceReauthenticationInitialization = FaceAuthenticationInitialization(authenticationToken: authToken, withImmediateRedirect: false)
+            idenfyController.initializeFaceAuthentication(faceAuthenticationInitialization: faceReauthenticationInitialization)
+            
+            let idenfyVC = idenfyController.instantiateNavigationController()
+            
+            idenfyVC.modalPresentationStyle = .fullScreen
+            
+            UIApplication.shared.windows.first?.rootViewController?.present(idenfyVC, animated: true)
+            
+            handleFaceReauthSdkCallbacks(idenfyController: idenfyController, resolver: resolve)
+            
+        } catch let error as NSError {
+            reject("error", error.domain, error)
+            return
+        } catch {
+            reject("error", "Unexpected error. Verify that config is structured correctly.", error)
+            return
         }
-
-        private func handleFaceReauthSdkCallbacks(idenfyController: IdenfyController, resolver resolve: @escaping RCTPromiseResolveBlock) {
-            idenfyController.handleIdenfyCallbacksForFaceAuthentication(faceAuthenticationResult: {
-                faceAuthenticationResult
-                        in
-                    let response = NativeResponseToReactNativeResponseMapper.mapFaceReauth(o: faceAuthenticationResult)
-                    resolve(response)
-                })
-            }
+    }
+    
+    private func handleFaceReauthSdkCallbacks(idenfyController: IdenfyController, resolver resolve: @escaping RCTPromiseResolveBlock) {
+        idenfyController.handleIdenfyCallbacksForFaceAuthentication(faceAuthenticationResult: {
+            faceAuthenticationResult
+            in
+            let response = NativeResponseToReactNativeResponseMapper.mapFaceReauth(o: faceAuthenticationResult)
+            resolve(response)
+        })
+    }
 }
