9bf19eabc01aedd2a55da2e37e59c8e430882c5e
viktas <viktas@idenfy.com>
2024-01-08 15:39:09 +0200

    2.3.3

--- example/src/Consts.tsx
+++ example/src/Consts.tsx
@@ -1,6 +1,7 @@
 export const BASE_URL = 'https://ivs.idenfy.com/';
 export const clientId = 'idenfySampleClientID';
-export const scanRef = 'PUT_YOUR_SCAN_REF';
 export const apiKey = 'PUT_YOUR_IDENFY_API_KEY_HERE';
 export const apiSecret = 'PUT_YOUR_IDENFY_API_SECRET_HERE';
+//Face authentication
+export const scanRef = 'PUT_YOUR_SCAN_REF';
 export const authenticationMethod = 'FACE_MATCHING';
